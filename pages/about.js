47f478e83c7d239f5be6bb2c617958cf581838b2
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2022-01-08 23:48:05 +0600

    learned layouts and css modules

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+const about = () => {
+    return (
+        <div>
+            <Head>
+                <title>about page title</title>
+            </Head>
+            about.js
+        </div>
+    );
+};
+
+export default about;
