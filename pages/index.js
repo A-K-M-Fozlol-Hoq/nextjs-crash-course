b2c19a561b72d2d13f8b1e70b6fdae7a67bf2bd9
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2022-01-09 17:30:10 +0600

    usint the api data [on progress]

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import {server} from '../config'
 import ArticleList from '../components/ArticleList';
 // import styles from '../styles/Home.module.css'
 
@@ -16,12 +16,23 @@ export default function Home({articles}) {
 }
 
 export const getStaticProps = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=6')
+  const res = await fetch(`${server}/api/articles`)
   const articles = await res.json()
-  
+
   return {
     props: {
       articles,
     },
   }
 }
+
+// export const getStaticProps = async () => {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=6')
+//   const articles = await res.json()
+  
+//   return {
+//     props: {
+//       articles,
+//     },
+//   }
+// }
