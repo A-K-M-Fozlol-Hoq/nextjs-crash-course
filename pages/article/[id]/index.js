c156d4532ec1e9c108642115aae54c479029dc8c
A-K-M-Fozlol-Hoq <akmfozlolhoq@gmail.com>
2022-01-09 12:51:49 +0600

    laarned fetch data with getStaticPaths

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -14,16 +14,45 @@ const Article = ({ article }) => {
   );
 };
 
-export const getServerSideProps = async (context) => {
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
-  );
-  const article = await res.json();
-  return {
-    props: {
-      article,
-    },
+// export const getServerSideProps = async (context) => {
+//   const res = await fetch(
+//     `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+//   );
+//   const article = await res.json();
+//   return {
+//     props: {
+//       article,
+//     },
+//   };
+// };
+
+
+export const getStaticProps = async (context) => {
+    const res = await fetch(
+      `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+    );
+    const article = await res.json();
+    return {
+      props: {
+        article,
+      },
+    };
   };
-};
+  
+  export const getStaticPaths = async () => {
+    const res = await fetch(
+      `https://jsonplaceholder.typicode.com/posts`
+    );
+    const article = await res.json();
+    const ids = article.map((article) => article.id);
+    const paths = ids.map((id) => ({
+      params: { id: id.toString() },
+    }));
+    return {
+        paths,
+        fallback: false,
+    }
+  };
+  
 
 export default Article;
